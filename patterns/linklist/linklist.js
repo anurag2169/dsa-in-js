d8cce1262eaa34b66209c91fdea3d5ccf259b591
ANURAG-ARMEDU <anurag@armedu.in>
2025-01-06 23:42:51 +0530

    add methods to linklist

--- patterns/linklist/linklist.js
+++ patterns/linklist/linklist.js
@@ -5,7 +5,28 @@ class node {
 }
 
 class linklist {
-  constructor() {    
+  constructor() {
     this.head = null;
+  }
+
+  add(data) {
+    const newNode = new node(data);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let temp = this.head;
+      while (temp.next) {
+        temp = temp.next;
+      }
+      temp.next = newNode;
+    }
+  }
+
+  print() {
+    let temp = this.head;
+    while (temp) {
+      console.log(temp.data);
+      temp = temp.next;
+    }
   }
 }
